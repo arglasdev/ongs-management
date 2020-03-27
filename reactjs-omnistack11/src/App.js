43ce6f78fbfcec276edeb0093c22f5368b653036
arglas <arglasdev@gmail.com>
2020-03-26 23:35:05 -0300

    logon page with css

---
+++ reactjs-omnistack11/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Logon from './pages/logon';
+
+import './global.css'
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+
+    <Logon/>
+
+    </div>
+  );
+}
+
+export default App;
